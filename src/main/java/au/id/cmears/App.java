6fa186bcd2761162526ec7ae2ccec92a5da66aaa
Chris Mears <chris@cmears.id.au>
2021-09-11 17:26:28 +1000

    Comments and tidying.

--- src/main/java/au/id/cmears/App.java
+++ src/main/java/au/id/cmears/App.java
@@ -9,10 +9,14 @@ public class App
         // int[] faces = {7,7,6,1,5,11,3,13,13,2,4,5,12, 3,7,11,8,6,10,7,10,5,2,13,6,1, 11,13,9,3,10,4,8,9,1,3,11,5,12, 4,12,4,8,9,8,12,2,9,6,1,10,2};
         // int[] faces = {1,3,7,3,5,11,8,6,11,12,9,3,6, 13,6,9,9,2,12,12,1,7,13,4,11,13, 7,8,2,10,4,5,8,1,6,8,10,13,4, 3,10,4,2,7,2,12,1,9,5,5,10,11};
         int[] faces = {4,6,12,13,2,5,11,4,13,7,10,1,13, 10,2,4,1,7,4,11,5,6,2,1,7,10, 6,6,3,9,13,11,1,8,2,8,8,9,12, 11,8,3,7,3,5,9,10,9,3,5,12,12};
+
+        // The quick-and-dirty method.
         // Search search = new Search(new Deal(faces));
         // while (true) {
         //     search.probe(search.initialState());
         // }
+
+        // The dynamic programming method.
         Dynamic d = new Dynamic(new Deal(faces));
         d.solve();
     }
--- src/main/java/au/id/cmears/Dynamic.java
+++ src/main/java/au/id/cmears/Dynamic.java
@@ -8,6 +8,11 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Dynamic {
+    // The cache maps a "position" in the game to a pair of (score, move).
+    // If we have
+    //   cache[position] = (score, move)
+    // it means "if you are looking at *position*, the best total score you can
+    // possibly get from here is *score*, and you can get it by playing *move*.
     record Entry(int score, ArrayList<Integer> piles) {}
     private HashMap<List<Integer>, Entry> cache;
     final Deal deal;
@@ -17,15 +22,23 @@ public class Dynamic {
         this.cache = new HashMap<List<Integer>, Entry>();
     }
 
-    Entry get(int[] consumed, int depth) {
-        String prefix = " ".repeat(2*depth);
-        // System.out.println(prefix + "Considering " + Arrays.toString(consumed) + " " + consumed);
+    // Get the Entry for a given position. If it's already in the cache, it's
+    // simply retrieved. If not, it will be computed, stored in the cache and
+    // then returned.
+    //
+    // *consumed* is a length-4 array saying how many cards have been already
+    // used up from each pile.  (This is the key for the cache.)
+    Entry get(int[] consumed) {
+        // The base case is that we've played all the cards and the game is
+        // over.
         if (Arrays.equals(consumed, new int[]{13,13,13,13})) {
             return new Entry(0, new ArrayList<Integer>());
         }
+        // We can't use the array directly as the cache key so we use a List
+        // version of it.
         List<Integer> key = IntStream.of(consumed).boxed().collect(Collectors.toList());
         if (!cache.containsKey(key)) {
-            // System.out.println(prefix + "Not in cache " + Arrays.toString(consumed) + " " + consumed);
+            // Construct a GameState from the position described by *consumed*.
             GameState state = new GameState();
             state.consumed = consumed.clone();
             state.deal = deal;
@@ -34,29 +47,41 @@ public class Dynamic {
             state.previousMove = null;
             state.previousState = null;
 
+            // For each possible move (a whole stack's worth of cards),
+            // we compute
+            //   (score of move) + (best score from resulting position)
+            // Then the best move is the move that maximises this value.
+
+            // Get all the possible moves.
             ArrayList<GameState> moves = Search.stackMoves(state);
 
+            // Iterate over all moves to find the best.
             GameState bestMove = moves.get(0);
-            int bestScore = bestMove.score + get(bestMove.consumed, depth+1).score();
+            int bestScore = bestMove.score + get(bestMove.consumed).score();
 
             for (GameState m: moves) {
-                int score = m.score + get(m.consumed, depth+1).score();
+                // The best move is the one that maximises this value.
+                int score = m.score + get(m.consumed).score();
                 if (score > bestScore) {
                     bestScore = score;
                     bestMove = m;
                 }
             }
 
+            // Put it in the cache.
             Entry e = new Entry(bestScore, bestMove.piles());
             cache.put(key, e);
-            // System.out.println("Put " + Arrays.toString(consumed) + " " + consumed + " = " + e);
         }
         return cache.get(key);
     }
 
     void solve() {
+        // We ask "what's the best move from the starting position?"
         int[] consumed = {0,0,0,0};
-        Entry e = get(consumed, 0);
+        Entry e = get(consumed);
+
+        // Then we follow the moves from there, each time consulting the cache
+        // to get the next move.
         System.out.println(e.score);
         while (!Arrays.equals(consumed, new int[]{13,13,13,13})) {
             System.out.println(e.piles());
@@ -65,7 +90,7 @@ public class Dynamic {
                     consumed[p]++;
                 }
             }
-            e = get(consumed, 0);
+            e = get(consumed);
         }
     }
 }
